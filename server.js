a74bd1057e1ccd720044e527538f52b47e8b56b8
lukaskwkw <lukaskwkw@gmail.com>
2015-12-31 19:22:27 +0100

    Regex added

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+var app = express();
+
+app.set('port',(process.env.PORT || 5000));
+
+app.get('/',function  (req,res) {
+	var sendBack = {
+ipaddress: req.ip,
+language: req.headers["accept-language"].match(/^.*?(?=,)/)[0],
+software: req.headers['user-agent'].match(/\((.*?)\)/)[1]
+	}
+	// console.log(req.headers);
+	res.end(JSON.stringify(sendBack));
+})
+
+app.listen(app.get('port'), function  () {
+	console.log("Server is running on %d port",app.get('port'));
+});
